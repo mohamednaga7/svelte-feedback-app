653b8dc3d92ba2402f497283e00f98a169ae3d7d
Mohamed Nagah <mohamed.nagah@cegedim.com>
2021-10-24 18:04:02 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import FeedbackList from "./components/FeedbackList.svelte";
+	import FeedbackStats from "./components/FeedbackStats.svelte";
+	import FeedbackForm from "./components/FeedbackForm.svelte";
+</script>
+
+<main class="container">
+	<FeedbackForm />
+	<FeedbackStats />
+	<FeedbackList />
+</main>
+
+<style>
+</style>
